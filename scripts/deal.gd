d91416d13f10cc37a69b3a9cff7657fff7c817a7
djalexkidd <alexpyr@outlook.fr>
2023-09-15 11:08:42 +0200

    bruh momento #69

--- scripts/deal.gd
+++ scripts/deal.gd
@@ -5,9 +5,7 @@ signal deal
 func _on_deal_button_1_pressed():
 	$DealButton1.disabled = true
 	deal.emit()
-	$DealButton1.hide()
 
 func _on_deal_button_2_pressed():
 	$DealButton2.disabled = true
 	deal.emit()
-	$DealButton2.hide()
